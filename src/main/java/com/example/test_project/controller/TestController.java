2875dcd889cb52d7784605fe864bc3e68c176f58
Himansha Nawarathna <102845827+HimanshaDe@users.noreply.github.com>
2025-01-01 17:03:50 +0530

    Initial commit

---
+++ src/main/java/com/example/test_project/controller/TestController.java
@@ -0,0 +1,12 @@
+package com.example.test_project.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin
+@RequestMapping("api/v1/test")
+public class TestController {
+
+ 
+
+}
