584dd6044120981e068d40d2c5c2a0aa666b1169
Himansha Nawarathna <102845827+HimanshaDe@users.noreply.github.com>
2025-02-07 10:17:25 +0530

    update UserController.java

--- src/main/java/com/example/test_project/controller/UserController.java
+++ src/main/java/com/example/test_project/controller/UserController.java
@@ -17,7 +17,11 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-
+    /**
+     * API to create a new user.
+     * @param userRequestDTO - The user details sent in the request body.
+     * @return ResponseEntity containing the status and response message.
+     */
     @PostMapping
     public ResponseEntity<ResponseDTO> createUser(@Valid @RequestBody UserRequestDTO userRequestDTO, BindingResult bindingResult){
         log.info("UserController.createUser() method accessed..");
@@ -25,16 +29,26 @@ public class UserController {
         return ResponseEntity.status(responseDTO.getStatus()).body(responseDTO);
     }
 
+    /**
+     * API to retrieve the list of all users.
+     * @return ResponseEntity containing the list of users.
+     */
     @GetMapping
     public ResponseEntity<ResponseDTO> getUsers(){
         log.info("UserController.getUsers() method accessed..");
         ResponseDTO responseDTO = userService.getUserList();
         return ResponseEntity.status(responseDTO.getStatus()).body(responseDTO);
     }
+
+    /**
+     * API to get details of a specific user by ID.
+     * @param id - The ID of the user to retrieve.
+     * @return ResponseEntity containing the course details.
+     */
     @GetMapping("/{id}")
-    public ResponseEntity<ResponseDTO> getUserById(@PathVariable("id") Integer userId){
+    public ResponseEntity<ResponseDTO> getUserById(@PathVariable Integer id){
         log.info("UserController.getUserById() method accessed..");
-        ResponseDTO responseDTO = userService.getUserById(userId);
+        ResponseDTO responseDTO = userService.getUserById(id);
         return ResponseEntity.status(responseDTO.getStatus()).body(responseDTO);
     }
 
